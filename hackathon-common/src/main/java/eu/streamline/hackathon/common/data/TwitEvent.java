201d07db8a3b2241fbb559c50d8092c8d20a7bec
fdmatos <francisco.dpa.matos@gmail.com>
2017-08-20 10:17:49 +0100

    Twit Event new fields

--- hackathon-common/src/main/java/eu/streamline/hackathon/common/data/TwitEvent.java
+++ hackathon-common/src/main/java/eu/streamline/hackathon/common/data/TwitEvent.java
@@ -14,18 +14,33 @@ public class TwitEvent implements Serializable {
     public String userScreenName;
     public int followersCount;
     public int friendsCount;
+    public String lang;
 
     public static TwitEvent fromString(String s) throws JSONException {
         JSONObject json = new JSONObject(s);
-
         TwitEvent event = new TwitEvent();
         event.id = (Long)json.get("id");
-        event.created_at = (String) json.get("created_at");
+        event.created_at = json.get("created_at").toString();
         event.text = (String) json.get("text");
         event.user = json.getJSONObject("user").getString("name");
         event.userScreenName = json.getJSONObject("user").getString("screen_name");
         event.followersCount = json.getJSONObject("user").getInt("followers_count");
         event.friendsCount = json.getJSONObject("user").getInt("friends_count");
+        event.lang = json.getString("lang");
         return event;
+    }
+
+    @Override
+    public String toString() {
+        return "TweetEvent{" +
+                "id=" + id +
+                ", created_at=" + created_at  +
+                ", text=" + text  +
+                ", user=" + user  +
+                ", userScreenName=" + userScreenName  +
+                ", followersCount=" + followersCount  +
+                ", friendsCount=" + friendsCount  +
+                ", lang=" + lang  +
+                '}';
     }
 }
