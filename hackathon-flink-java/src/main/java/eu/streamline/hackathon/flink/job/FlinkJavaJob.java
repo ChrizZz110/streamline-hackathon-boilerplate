ec7dbd96bffa7efdb5b189706430dbc92092bc49
ChrizZz110 <110@posteo.de>
2017-08-20 14:23:27 +0200

    added bubble chart

--- hackathon-flink-java/src/main/java/eu/streamline/hackathon/flink/job/FlinkJavaJob.java
+++ hackathon-flink-java/src/main/java/eu/streamline/hackathon/flink/job/FlinkJavaJob.java
@@ -7,17 +7,20 @@ import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.contrib.streaming.DataStreamUtils;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.util.Collector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
+import java.io.*;
+import java.util.*;
 
 /**
  * Flink Java Job class
@@ -36,7 +39,7 @@ public class FlinkJavaJob {
         ParameterTool params = ParameterTool.fromArgs(args);
 
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime);
 
         // Read the twitter authentication data
         Properties props = new Properties();
@@ -47,7 +50,7 @@ public class FlinkJavaJob {
         // Create the data source
         DataStream<String> streamSource = env.addSource(new MyTwitterSource(props));
 
-        streamSource
+        DataStream<TwitEvent> tweetEvents = streamSource
                 .filter(new FilterFunction<String>() {
                     @Override
                     public boolean filter(String s) throws Exception {
@@ -61,12 +64,95 @@ public class FlinkJavaJob {
                     }
                 });
 
-        streamSource.print();
+        DataStream<Tuple2<String, Integer>> hashtagSum = tweetEvents
+                .flatMap(new FlatMapFunction<TwitEvent, Tuple2<String, Integer>>() {
+                    @Override
+                    public void flatMap(TwitEvent twitEvent, Collector<Tuple2<String, Integer>> collector) throws Exception {
+                        for (String hashtag : Extractor.extractTags(twitEvent.text)) {
+                            collector.collect(new Tuple2<String, Integer>(hashtag, 1));
+                        }
+                    }
+                })
+                .keyBy(0)
+                .window(TumblingProcessingTimeWindows.of(Time.seconds(10)))
+                .sum(1);
 
-        try {
-            env.execute("Flink Java Twitter Analyzer");
-        } catch (Exception e) {
-            LOG.error("Failed to execute Flink job {}", e);
+        Iterator<Tuple2<String, Integer>> tuple2Iterator = DataStreamUtils.collect(hashtagSum);
+
+        File fileHt = new File("hashtags.csv");
+        FileWriter fileWriterHt = new FileWriter(fileHt, false);
+        StringBuilder stringBuilderHt = new StringBuilder();
+        stringBuilderHt.append("id,value\n");
+
+        long startDateHt = System.currentTimeMillis();
+
+        while (tuple2Iterator.hasNext()) {
+            Tuple2<String, Integer> next = tuple2Iterator.next();
+            long tempDate = System.currentTimeMillis();
+            if (tempDate - startDateHt > 10000) {
+                startDateHt = System.currentTimeMillis();
+                fileWriterHt = new FileWriter(fileHt, false);
+                fileWriterHt.write(stringBuilderHt.toString());
+                fileWriterHt.flush();
+                stringBuilderHt = new StringBuilder();
+                stringBuilderHt.append("id,value\n");
+            }
+            stringBuilderHt.append(next.f0);
+            stringBuilderHt.append(",");
+            stringBuilderHt.append(next.f1);
+            stringBuilderHt.append("\n");
+
         }
+        fileWriterHt.close();
+
+
+        DataStream<Tuple3<Integer, String, Integer>> sum = tweetEvents
+                .flatMap(new FlatMapFunction<TwitEvent, Tuple3<Integer, String, Integer>>() {
+                    @Override
+                    public void flatMap(TwitEvent twitEvent, Collector<Tuple3<Integer, String, Integer>> collector) throws Exception {
+                        for (String mention : Extractor.extractMentions(twitEvent.text)) {
+                            collector.collect(new Tuple3<Integer, String, Integer>(0, mention, 1));
+                        }
+                    }
+                })
+                .keyBy(1)
+                .window(TumblingProcessingTimeWindows.of(Time.seconds(10)))
+                .sum(2);
+
+        Iterator<Tuple3<Integer, String, Integer>> tuple3Iterator = DataStreamUtils.collect(sum);
+
+        File file = new File("mentions.csv");
+        FileWriter fileWriter = new FileWriter(file, false);
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("id,value\n");
+
+
+        long startDate = System.currentTimeMillis();
+
+        while (tuple3Iterator.hasNext()) {
+            Tuple3<Integer, String, Integer> next = tuple3Iterator.next();
+            long tempDate = System.currentTimeMillis();
+            if (tempDate - startDate > 10000) {
+                System.out.println("Mentions written.");
+                startDate = System.currentTimeMillis();
+                fileWriter = new FileWriter(file, false);
+                fileWriter.write(stringBuilder.toString());
+                fileWriter.flush();
+                stringBuilder = new StringBuilder();
+                stringBuilder.append("id,value\n");
+            }
+            stringBuilder.append(next.f1);
+            stringBuilder.append(",");
+            stringBuilder.append(next.f2);
+            stringBuilder.append("\n");
+
+        }
+        fileWriter.close();
+
+//        try {
+//            env.execute("Flink Java Twitter Analyzer");
+//        } catch (Exception e) {
+//            LOG.error("Failed to execute Flink job {}", e);
+//        }
     }
 }
--- hackathon-flink-java/src/main/java/eu/streamline/hackathon/flink/operations/Extractor.java
+++ hackathon-flink-java/src/main/java/eu/streamline/hackathon/flink/operations/Extractor.java
@@ -18,28 +18,25 @@ public class Extractor {
     }
 
     public static ArrayList<String> extractTags(String input){
-        String[] stringArray = input.split(" ");
         ArrayList<String> finalArrayList = new ArrayList<>();
-        for(int i = 0; i<stringArray.length; i++){
-            String tmp = stringArray[i];
-            if(tmp.contains("#")){
-                finalArrayList.add(stringArray[i]);
-            }
+
+        Pattern pattern = Pattern.compile("#[a-zA-Z0-9]+");
+        Matcher matcher = pattern.matcher(input);
+        // check all occurance
+        while (matcher.find()) {
+            finalArrayList.add(matcher.group());
         }
         return finalArrayList;
     }
 
     public static ArrayList<String> extractMentions(String input){
-        String[] stringArray = input.split(" ");
         ArrayList<String> finalArrayList = new ArrayList<>();
-        for(int i = 0; i<stringArray.length; i++){
-            String tmp = stringArray[i];
-            if(tmp.contains("@")){
-                finalArrayList.add(stringArray[i]);
-            }
+        Pattern pattern = Pattern.compile("@[a-zA-Z0-9]+");
+        Matcher matcher = pattern.matcher(input);
+        // check all occurance
+        while (matcher.find()) {
+            finalArrayList.add(matcher.group());
         }
         return finalArrayList;
     }
-
-
 }
